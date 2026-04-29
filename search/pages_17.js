889ebaf0bedbae9f587602da058f0674d64f7beb
leissa <leissa@users.noreply.github.com>
2026-04-29 16:35:38 +0000

    deploy: mimir/mimir@fd06714b5fdf8860addb062259fa3403fcaa37fa

--- search/pages_17.js
+++ search/pages_17.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['zero_0',['%autodiff.zero',['../autodiff.html#autotoc_md118',1,'']]],
-  ['zip_1',['zip',['../tuple.html#autotoc_md341',1,'%tuple.zip'],['../vec.html#autotoc_md345',1,'%vec.zip']]]
+  ['zero_0',['%autodiff.zero',['../autodiff.html#autotoc_md117',1,'']]],
+  ['zip_1',['zip',['../tuple.html#autotoc_md340',1,'%tuple.zip'],['../vec.html#autotoc_md344',1,'%vec.zip']]]
 ];
